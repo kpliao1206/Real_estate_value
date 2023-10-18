39218b8a16d697ce3e7e9d28181cb0ebe3076efd
kpliao1206 <98268520+kpliao1206@users.noreply.github.com>
2023-10-18 17:07:43 +0800

    2023/10/18

--- Dataset.py
+++ Dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
 
 class Features12_dataset(Dataset):
     def __init__(self, X, y, train=True, random_state=12):
@@ -14,7 +15,11 @@ class Features12_dataset(Dataset):
         self.scaler_y = FunctionTransformer(np.log1p, np.expm1) # log(x+1), 使資料更接近常態分佈 inverse_func: np.expm1
 
         if train:
-            scaler_X = StandardScaler()
+            # scaler_X = StandardScaler()
+            scaler_X = Pipeline([
+                ('MinMax', MinMaxScaler()),
+                ('log1p', FunctionTransformer(np.log1p, np.expm1))     
+            ])
             scaler_X.fit(X_train)
             X_scaled_train = scaler_X.transform(X_train)
             self.input = X_scaled_train
@@ -23,7 +28,11 @@ class Features12_dataset(Dataset):
             # self.scaler_y.fit(y_train)
             self.target = self.scaler_y.transform(y_train)
         else:
-            scaler_X = StandardScaler()
+            # scaler_X = StandardScaler()
+            scaler_X = Pipeline([
+                ('MinMax', MinMaxScaler()),
+                ('log1p', FunctionTransformer(np.log1p, np.expm1))     
+            ])
             scaler_X.fit(X_test)
             X_scaled_test = scaler_X.transform(X_test)
             self.input = X_scaled_test
